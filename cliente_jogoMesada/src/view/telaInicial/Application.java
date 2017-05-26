980da18f287c964be734fb0e348d5479cbc60cc6
Wanderson <bsilva.wanderson@gmail.com>
2017-05-26 13:19:56 -0300

    matando a thread ao fechar a tela inicial

--- cliente_jogoMesada/src/view/telaInicial/Application.java
+++ cliente_jogoMesada/src/view/telaInicial/Application.java
@@ -1,9 +1,12 @@
 package view.telaInicial;
 
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * Created by wanderson on 25/05/17.
@@ -16,6 +19,13 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
         Parent root = FXMLLoader.load(getClass().getResource("FXMLTelaInicial.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
